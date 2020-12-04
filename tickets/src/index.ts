806557808baaa8354ad4d95d1336b1d963839603
Steven Hunter <steven.x.hunter@googlemail.com>
2020-12-04 07:41:25 +0000

    Added tickets microservice

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,8 @@ const start = async () =>  {
   if (!process.env.JWT_KEY){
     throw new Error('Env var JWT_KEY must be defined');
   }
-  if (!process.env.MONGO_URI){
+  
+  if (!process.env.MONGO_URI) {
     throw new Error('MONGO_URI must be defined');
   }
 
